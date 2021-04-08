25659dcfdc774ba08e5475c6f48419431507475b
lux <andreas.mueller@exxcellent.de>
2021-04-08 14:55:18 +0200

    chanage version to 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <button onClick={() => setMore(!more)}>TEST BUTTON</button>
         {more && <div style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "red", width: "100%", height: "100vh", color: "green"}}>
             <label>!!! JETZT WIRDS WILD  !!!</label>
-            <label>!!! Version 1.1  !!!</label>
+            <label>!!! Version 2.0  !!!</label>
         </div>}
     </div>
   );
