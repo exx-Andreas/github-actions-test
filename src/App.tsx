06d9935d8e4269a7aee3af4b93036924520cb745
lux <andreas.mueller@exxcellent.de>
2021-04-08 14:52:00 +0200

    change page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <button onClick={() => setMore(!more)}>TEST BUTTON</button>
         {more && <div style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "red", width: "100%", height: "100vh", color: "green"}}>
             <label>!!! JETZT WIRDS WILD  !!!</label>
+            <label>!!! Version 1.1  !!!</label>
         </div>}
     </div>
   );
