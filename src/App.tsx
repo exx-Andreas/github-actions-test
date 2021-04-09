7a4f0207f95b8b514b0cee5e30f01e38b20c3cac
lux <andreas.mueller@exxcellent.de>
2021-04-09 09:18:32 +0200

    dev deploy changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+export function App() {
 
     const [more, setMore] = useState(false);
 
@@ -11,10 +11,8 @@ function App() {
       <button onClick={() => setMore(!more)}>TEST BUTTON</button>
         {more && <div style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "red", width: "100%", height: "100vh", color: "green"}}>
             <label>!!! JETZT WIRDS WILD  !!!</label>
-            <label>!!! Version 2.0  !!!</label>
+            <label>!!! Dev deploy with feature branch  !!!</label>
         </div>}
     </div>
   );
 }
-
-export default App;
